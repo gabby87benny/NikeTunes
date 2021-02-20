4736a5f17e4d23d6c27a0fe6b7caa5fe48ff5841
Gabriel Benny <gabby87benny@gmail.com>
2021-02-19 23:21:01 -0600

    Added network layer along with loading spinner

--- NikeTunes/Models/NKAlbum.swift
+++ NikeTunes/Models/NKAlbum.swift
@@ -7,6 +7,27 @@
 
 import Foundation
 
-struct NKAlbum: Codable {
+struct Main: Codable {
+    let feed: Content
+}
 
+struct Content: Codable {
+    let title: String
+    let id: String
+    let author: [String: String]
+    let results: [NKAlbum]
+}
+
+struct NKAlbum: Codable {
+    let artistName: String
+    let name: String
+    let artworkUrl100: String
+    let releaseDate: String
+    let copyright: String
+    let genres: [OneGenre]
+    let url: String
+}
+
+struct OneGenre: Codable {
+    let name: String
 }
--- NikeTunes/ViewModels/NKAlbumsViewModel.swift
+++ NikeTunes/ViewModels/NKAlbumsViewModel.swift
@@ -7,3 +7,51 @@
 
 import Foundation
 
+class NKAlbumsViewModel {
+    private var albums = [NKAlbum]()
+    private let mobileService: NKTunesAPIManager_Protocol
+    typealias CompletionResult = ([NKAlbum], Error?) -> ()
+    
+    init(albums: [NKAlbum] = [], mobileService: NKTunesAPIManager_Protocol = NKTunesAPIManager()) {
+        self.albums = albums
+        self.mobileService = mobileService
+    }
+    
+    func fetchAlbums(completion: @escaping CompletionResult) {
+        self.mobileService.getAlbums {[weak self] result in
+            switch result {
+                case .failure(let error):
+                    print(error)
+                    completion([], error)
+                case .success(let albums):
+                    self?.albums.append(contentsOf: albums)
+                    completion(albums, nil)
+            }
+        }
+    }
+    
+    /**
+    Returns count of albums.
+     
+    - Returns: Number of rows.
+    */
+    
+    func albumsCount() -> Int {
+        return self.albums.count
+    }
+
+    /**
+    Returns album info at indexpath.
+
+    - Parameters:
+       - indexPath: The indexpath to look for
+     
+    - Returns: The corresponding album info at indexpath.
+    */
+    
+    func album(at indexPath: IndexPath) -> NKAlbum? {
+        guard indexPath.row < albums.count  else { return nil }
+        return albums[indexPath.row]
+    }
+}
+
